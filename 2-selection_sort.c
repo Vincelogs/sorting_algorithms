aa17c7e8f5433f1aa1f5356ef69bfc942cfa06f0
vincelogs <vincentharyor@gmail.com>
2023-01-24 08:23:25 -0800

    selection algo

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - A function that use selection sort algorithm.
+ * @array: An array to sort.
+ * @size: The size of the array.
+ * Return: Nothing.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i = 0, j = 0, min_idx = 0;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
